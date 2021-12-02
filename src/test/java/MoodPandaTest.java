4c695fc8074f895787c901aadffb5f7466fcd0d8
Ihar-Malinouski <Ghjcnbghjqnb126>
2021-12-03 00:27:49 +0300

    update test path 3

--- src/test/java/MoodPandaTest.java
+++ src/test/java/MoodPandaTest.java
@@ -1,4 +1,9 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
+import io.qameta.allure.TmsLink;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.FeedPage;
 import steps.FeedStep;
@@ -8,6 +13,11 @@ import steps.RateYourHappinessModalStep;
 public class MoodPandaTest extends BaseTest {
 
     @Test(description = "Проверка валидности выбранного значения настроения")
+    @Description("Some detailed test description")
+    @Parameters({"login", "password", "secondNameProduct"})
+    @Link("TMS-1")
+    @Issue("TMS-2")
+    @TmsLink("TMS-3")
     public void moodTest() {
         LoginStep.authotization();
         FeedStep.waitAndClickMoodButton()
@@ -17,6 +27,11 @@ public class MoodPandaTest extends BaseTest {
     }
 
     @Test(description = "Проверка сохранения с верной измененной датой при заполнении настроения")
+    @Description("Some detailed test description")
+    @Parameters({"login", "password", "secondNameProduct"})
+    @Link("TMS-1")
+    @Issue("TMS-2")
+    @TmsLink("TMS-3")
     public void checkDateMoodTest() {
         LoginStep.authotization();
         FeedStep.waitAndClickMoodButton()
@@ -28,18 +43,23 @@ public class MoodPandaTest extends BaseTest {
         Assert.assertEquals(FeedPage.getDateMood(), "2 days ago");
     }
 
-    @Test(description = "Проверка ввода Reply под оценкой настроения")
-    public void inputReplyTest() {
-        LoginStep.authotization();
-        FeedStep.waitAndClickMoodButton()
-                .updateMood(4)
-                .openCalendarButton()
-                .chooseMonth("Apr")
-                .openYearCalendar()
-                .chooseYear("2015");
-        RateYourHappinessModalStep.updateMoodAndGoToMyDiary();
-        feedPage.getFirstMoodReplyMessage("Чот настроенице такое себе :)")
-                .clickReplyButton();
-        Assert.assertEquals(FeedPage.getFirstReplyText(), "Чот настроенице такое себе :)");
-    }
+//    @Test(description = "Проверка ввода Reply под оценкой настроения")
+//    @Description("Some detailed test description")
+//    @Parameters({"login", "password", "secondNameProduct"})
+//    @Link("TMS-1")
+//    @Issue("TMS-2")
+//    @TmsLink("TMS-3")
+//    public void inputReplyTest() {
+//        LoginStep.authotization();
+//        FeedStep.waitAndClickMoodButton()
+//                .updateMood(4)
+//                .openCalendarButton()
+//                .chooseMonth("Apr")
+//                .openYearCalendar()
+//                .chooseYear("2015");
+//        RateYourHappinessModalStep.updateMoodAndGoToMyDiary();
+//        feedPage.getFirstMoodReplyMessage("Чот настроенице такое себе :)")
+//                .clickReplyButton();
+//        Assert.assertEquals(FeedPage.getFirstReplyText(), "Чот настроенице такое себе :)");
+//    }
 }
